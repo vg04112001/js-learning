3a7c935831d1258dffd278e53070509268cfece1
vg04112001 <110903149+vg04112001@users.noreply.github.com>
2024-10-07 04:52:45 +0000

    added comment

--- 01_basics/variables/test.js
+++ 01_basics/variables/test.js
@@ -3,4 +3,8 @@ let email = "a@b.com"
 var clientName = "Viraj"
 withoutKeyword = "works"
 
+/*
+    Prefer not to use var because of issue in block and functional scope
+*/
+
 console.table([id, email, clientName, withoutKeyword]);
